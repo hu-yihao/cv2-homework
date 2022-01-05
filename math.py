22fd878771cfa6ccaf445e2dd89062ace51d5992
hyh <867039866@qq.com>
2022-01-05 19:57:04 +0800

    实现图像的八进制读入，取零值函数

---
+++ math.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+
+Min_Area = 2000  # 车牌区域允许最大面积
+
+"""
+    该文件包含读文件函数
+    取零值函数
+    矩阵校正函数
+    颜色判断函数
+"""
+
+
+def img_read(filename):
+    '''
+        以uint8方式读取filename
+        放入imdecode中，cv2.IMREAD_COLOR读取彩色照片
+    '''
+
+    # cv2.IMREAD_COLOR：读入一副彩色图像。图像的透明度会被忽略，这是默认参数
+    # cv2.IMREAD_GRAYSCALE：以灰度模式读入图像
+    # cv2.IMREAD_UNCHANGED：读入一幅图像，并且包括图像的 alpha 通道
+    return cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+
+def find_waves(threshold, histogram):
+    up_point = -1  # 上升点
+    is_peak = False
+    if histogram[0] > threshold:
+        up_point = 0
+        is_peak = True
+    wave_peaks = []
+    for i, x in enumerate(histogram):
+        if is_peak and x < threshold:
+            if i - up_point > 2:
+                is_peak = False
+                wave_peaks.append((up_point, i))
+        elif not is_peak and x >= threshold:
+            is_peak = True
+            up_point = i
+    if is_peak and up_point != -1 and i - up_point > 4:
+        wave_peaks.append((up_point, i))
+    return wave_peaks
+
+
+def point_limit(point):
+    if point[0] < 0:
+        point[0] = 0
+    if point[1] < 0:
+        point[1] = 0
+
+
+def accurate_place(card_img_hsv, limit1, limit2, color):
+    row_num, col_num = card_img_hsv.shape[:2]
+    xl = col_num
+    xr = 0
+    yh = 0
+    yl = row_num
+    row_num_limit = 21
+    col_num_limit = col_num * 0.8 if color != "green" else col_num * 0.5  # 绿色有渐变
+    for i in range(row_num):
+        count = 0
+        for j in range(col_num):
+            H = card_img_hsv.item(i, j, 0)
+            S = card_img_hsv.item(i, j, 1)
+            V = card_img_hsv.item(i, j, 2)
+            if limit1 < H <= limit2 and 34 < S and 46 < V:
+                count += 1
+        if count > col_num_limit:
+            if yl > i:
+                yl = i
+            if yh < i:
+                yh = i
+    for j in range(col_num):
+        count = 0
+        for i in range(row_num):
+            H = card_img_hsv.item(i, j, 0)
+            S = card_img_hsv.item(i, j, 1)
+            V = card_img_hsv.item(i, j, 2)
+            if limit1 < H <= limit2 and 34 < S and 46 < V:
+                count += 1
+        if count > row_num - row_num_limit:
+            if xl > j:
+                xl = j
+            if xr < j:
+                xr = j
+    return xl, xr, yh, yl
