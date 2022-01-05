3b5f2689ef5cedf6b00787b6b2995cff505cabc8
hyh <867039866@qq.com>
2022-01-05 22:28:19 +0800

    实现图像处理，找到车牌，实现math中的方法

--- function.py
+++ function.py
@@ -72,6 +72,157 @@ class CardPredictor:
         cv2.imwrite("tmp/img_xingtai.jpg", img_edge2)
         return img_edge2, oldimg
 
+    def img_only_color(self, filename, oldimg, img_contours):
+        """
+        :param filename: 图像文件
+        :param oldimg: 原图像文件
+        :return: 识别到的字符、定位的车牌图像、车牌颜色
+        """
+        pic_hight, pic_width = img_contours.shape[:2]  # #取彩色图片的高、宽
+
+        lower_blue = np.array([100, 110, 110])
+        upper_blue = np.array([130, 255, 255])
+        lower_yellow = np.array([15, 55, 55])
+        upper_yellow = np.array([50, 255, 255])
+        lower_green = np.array([50, 50, 50])
+        upper_green = np.array([100, 255, 255])
+
+        # BGR ---> HSV
+        hsv = cv2.cvtColor(filename, cv2.COLOR_BGR2HSV)
+        # 利用cv2.inRange函数设阈值，去除背景部分
+        # 参数1：原图
+        # 参数2：图像中低于值，图像值变为0
+        # 参数3：图像中高于值，图像值变为0
+        mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+        mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        mask_green = cv2.inRange(hsv, lower_yellow, upper_green)
+
+        # 图像算术运算  按位运算 按位操作有： AND， OR， NOT， XOR 等
+        output = cv2.bitwise_and(hsv, hsv, mask=mask_blue + mask_yellow + mask_green)
+        # 根据阈值找到对应颜色
+
+        output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+        Matrix = np.ones((20, 20), np.uint8)
+        # 使用一个 20x20 的卷积核
+        img_edge1 = cv2.morphologyEx(output, cv2.MORPH_CLOSE, Matrix)  # 闭运算
+        img_edge2 = cv2.morphologyEx(img_edge1, cv2.MORPH_OPEN, Matrix)  # 开运算
+
+        card_contours = img_math.img_findContours(img_edge2)
+        card_imgs = img_math.img_Transform(card_contours, oldimg, pic_width, pic_hight)
+        colors, car_imgs = img_math.img_color(card_imgs)
+
+        predict_result = []
+        predict_str = ""
+        roi = None
+        card_color = None
+
+        for i, color in enumerate(colors):
+
+            if color in ("blue", "yello", "green"):
+                card_img = card_imgs[i]
+
+                try:
+                    gray_img = cv2.cvtColor(card_img, cv2.COLOR_BGR2GRAY)
+                except:
+                    print("gray转换失败")
+
+                # 黄、绿车牌字符比背景暗、与蓝车牌刚好相反，所以黄、绿车牌需要反向
+                if color == "green" or color == "yello":
+                    gray_img = cv2.bitwise_not(gray_img)
+                ret, gray_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                x_histogram = np.sum(gray_img, axis=1)
+
+                x_min = np.min(x_histogram)
+                x_average = np.sum(x_histogram) / x_histogram.shape[0]
+                x_threshold = (x_min + x_average) / 2
+                wave_peaks = img_math.find_waves(x_threshold, x_histogram)
+
+                if len(wave_peaks) == 0:
+                    # print("peak less 0:")
+                    continue
+                # 认为水平方向，最大的波峰为车牌区域
+                wave = max(wave_peaks, key=lambda x: x[1] - x[0])
+
+                gray_img = gray_img[wave[0]:wave[1]]
+                # 查找垂直直方图波峰
+                row_num, col_num = gray_img.shape[:2]
+                # 去掉车牌上下边缘1个像素，避免白边影响阈值判断
+                gray_img = gray_img[1:row_num - 1]
+                y_histogram = np.sum(gray_img, axis=0)
+                y_min = np.min(y_histogram)
+                y_average = np.sum(y_histogram) / y_histogram.shape[0]
+                y_threshold = (y_min + y_average) / 5  # U和0要求阈值偏小，否则U和0会被分成两半
+                wave_peaks = img_math.find_waves(y_threshold, y_histogram)
+                if len(wave_peaks) < 6:
+                    # print("peak less 1:", len(wave_peaks))
+                    continue
+
+                wave = max(wave_peaks, key=lambda x: x[1] - x[0])
+                max_wave_dis = wave[1] - wave[0]
+                # 判断是否是左侧车牌边缘
+                if wave_peaks[0][1] - wave_peaks[0][0] < max_wave_dis / 3 and wave_peaks[0][0] == 0:
+                    wave_peaks.pop(0)
+
+                # 组合分离汉字
+                cur_dis = 0
+                for i, wave in enumerate(wave_peaks):
+                    if wave[1] - wave[0] + cur_dis > max_wave_dis * 0.6:
+                        break
+                    else:
+                        cur_dis += wave[1] - wave[0]
+                if i > 0:
+                    wave = (wave_peaks[0][0], wave_peaks[i][1])
+                    wave_peaks = wave_peaks[i + 1:]
+                    wave_peaks.insert(0, wave)
+
+                point = wave_peaks[2]
+                point_img = gray_img[:, point[0]:point[1]]
+                if np.mean(point_img) < 255 / 5:
+                    wave_peaks.pop(2)
+
+                if len(wave_peaks) <= 6:
+                    # print("peak less 2:", len(wave_peaks))
+                    continue
+                # print(wave_peaks)
+
+                # wave_peaks  车牌字符 类型列表 包含7个（开始的横坐标，结束的横坐标）
+
+                part_cards = img_math.seperate_card(gray_img, wave_peaks)
+
+                for i, part_card in enumerate(part_cards):
+                    # 可能是固定车牌的铆钉
+
+                    if np.mean(part_card) < 255 / 5:
+                        # print("a point")
+                        continue
+                    part_card_old = part_card
+
+                    w = abs(part_card.shape[1] - SZ) // 2
+
+                    part_card = cv2.copyMakeBorder(part_card, 0, 0, w, w, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+                    part_card = cv2.resize(part_card, (SZ, SZ), interpolation=cv2.INTER_AREA)
+
+                    part_card = img_recognition.preprocess_hog([part_card])
+                    if i == 0:
+                        resp = self.modelchinese.predict(part_card)
+                        charactor = img_recognition.provinces[int(resp[0]) - PROVINCE_START]
+                    else:
+                        resp = self.model.predict(part_card)
+                        charactor = chr(resp[0])
+                    # 判断最后一个数是否是车牌边缘，假设车牌边缘被认为是1
+                    if charactor == "1" and i == len(part_cards) - 1:
+                        if part_card_old.shape[0] / part_card_old.shape[1] >= 7:  # 1太细，认为是边缘
+                            continue
+                    predict_result.append(charactor)
+                    predict_str = "".join(predict_result)
+
+                roi = card_img
+                card_color = color
+                break
+
+        cv2.imwrite("tmp/img_caijian.jpg", roi)
+        return predict_str, roi, card_color  # 识别到的字符、定位的车牌图像、车牌颜色
+
 
 
 
